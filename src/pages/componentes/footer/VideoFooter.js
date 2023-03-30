554e5af2fdbf13c66ea93dfafe3fd9ce74b4d79a
Eduardo Andrade <109284750+eduardoandraade@users.noreply.github.com>
2023-03-30 20:09:50 -0300

    incluindo o footer
    criando componentes e usando lib de icones

---
+++ src/pages/componentes/footer/VideoFooter.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./videoFooter.css";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+
+function VideoFooter() {
+  return (
+    <div className="videoFooter">
+      <div className="videoFooter__text">
+        <h3>@eduardoandraade</h3>
+        <p>Descrição do Video</p>
+        <div className="videofooter__music">
+          <MusicNoteIcon className="videoFooter__icon" />
+          <div className="videoFooterMusic__text">
+            <p>Titulo da música</p>
+          </div>
+        </div>
+      </div>
+      <img
+        className="videoFooter__record"
+        alt="Imagem de um vinil girando"
+        src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/vinil.png"
+      />
+    </div>
+  );
+}
+
+export default VideoFooter;
