b4ae5f927a5d022068b8704df4c62f18b810fb35
Eduardo Andrade <109284750+eduardoandraade@users.noreply.github.com>
2023-03-31 19:59:36 -0300

    instalando sidebar

--- src/App.js
+++ src/App.js
@@ -5,10 +5,26 @@ function App() {
   return (
     <div className="App">
       <div className='app__videos'>
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video 
+          likes={111}
+          messages={222}
+          shares={333}
+          name="João"
+          description="Cat de Goleiro"
+          music="musica épica"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
+        />
+
+
+        <Video 
+          likes={1325}
+          messages={228}
+          shares={40}
+          name="eduardoandraade"
+          description="gatito olhando para a camera"
+          music="music - clap your hands"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"
+        />
       </div>
     </div>
   );
